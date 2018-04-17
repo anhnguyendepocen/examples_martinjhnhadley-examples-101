ada61acb13ab5dea2e3ba64fb78eb4e31b28587a
martinjhnhadley <martinjohnhadley@gmail.com>
2018-04-17 07:21:33 +0100

    removed processing script from got-network

--- got-network/data-raw/make_GoT_data.R
+++ got-network/data-raw/make_GoT_data.R
@@ -13,29 +13,4 @@ load("data-raw/union_edges.RData")
 
 got_characters <- union_characters %>%
   mutate_if(is.factor, as.character)
-got_edges <- union_edges
-
-got_characters %>% colnames()
-
-got_characters <- got_characters %>%
-  select(-shape, -popularity, -color) %>%
-  mutate(id = name) %>%
-  mutate(label = name,
-         title = name) %>%
-  mutate(culture = ifelse(is.na(culture), "Unknown Culture", culture)) %>%
-  mutate(gender = recode(male, `0` = "female", `1` = "male")) %>%
-  select(-male) %>%
-  select(id, everything())
-
-got_supercultures <- read_csv("data-raw/got_super-cultures.csv")
-
-got_characters <- got_characters %>%
-  mutate(superculture = plyr::mapvalues(culture, from = got_supercultures$culture, to = got_supercultures$super.culture))
-
-got_edges <- got_edges %>%
-  select(-lty, -color) %>%
-  rename(from = source,
-         to = target)
-
-write_csv(got_edges, "data/GoT_edges.csv")
-write_csv(got_characters, "data/GoT_nodes.csv")
+got_edges <- union_edgesw
