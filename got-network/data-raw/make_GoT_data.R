031d3a72f6ef68580a8d0cb5189c781081cb99bf
Martin John Hadley <martinjohnhadley@gmail.com>
2019-04-11 08:07:29 +0100

    commented out download code

--- got-network/data-raw/make_GoT_data.R
+++ got-network/data-raw/make_GoT_data.R
@@ -1,16 +1,15 @@
 library("tidyverse")
 ## Data source: https://github.com/ShirinG/blog_posts_prep/tree/master/GoT
 ## Additional info: Data sources from https://www.kaggle.com/mylesoneill/game-of-thrones and manipulated by hand by https://shiring.github.io/
-
-download.file("https://github.com/ShirinG/blog_posts_prep/blob/master/GoT/union_characters.RData?raw=true",
-              destfile = "data-raw/union_characters.RData")
-download.file("https://github.com/ShirinG/blog_posts_prep/blob/master/GoT/union_edges.RData?raw=true",
-              destfile = "data-raw/union_edges.RData")
-
-load("data-raw/union_characters.RData")
-load("data-raw/union_edges.RData")
-
-
-got_characters <- union_characters %>%
-  mutate_if(is.factor, as.character)
-got_edges <- union_edgesw
+## The code below has been deliberately double commented due to 
+## cross-platform compatibility issues experienced in the past.
+## ## download.file("https://github.com/ShirinG/blog_posts_prep/blob/master/GoT/union_characters.RData?raw=true",
+## ##             destfile = "data-raw/union_characters.RData")
+## ## download.file("https://github.com/ShirinG/blog_posts_prep/blob/master/GoT/union_edges.RData?raw=true",
+## ##             destfile = "data-raw/union_edges.RData")
+## ## load("data-raw/union_characters.RData")
+## ## load("data-raw/union_edges.RData")
+## ## union_characters %>%
+## ##   write_csv("data-raw/union_characters.csv")
+## ## union_edges %>%
+## ##   write_csv("data-raw/union_edges.csv")
